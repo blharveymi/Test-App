9fb9b68462d470dc852cc4b41180e3f9bdafcf39
Brandon Harvey <brandonharvey@Brandons-iMac.local>
2015-05-23 10:49:03 -0400

    Initial Commit

---
+++ Test App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test App
+//
+//  Created by Brandon Harvey on 5/23/15.
+//  Copyright (c) 2015 Brandon Harvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
